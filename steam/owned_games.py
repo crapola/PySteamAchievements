5225b355183bbbe57ada114b9f91ca4ad4676d4a
Crapola <crapola@users.noreply.github.com>
2022-02-08 19:25:12 +0100

    Fix keys

--- steam/owned_games.py
+++ steam/owned_games.py
@@ -9,7 +9,7 @@ def owned_games(steam_path:str,account_id)->tuple:
 	> account_id: These are the four lower bytes of the Steam ID.
 	"""
 	stuff=vdf.load(open(f"{steam_path}/userdata/{account_id}/config/localconfig.vdf","r",encoding="utf-8"))
-	apps=stuff["UserLocalConfigStore"]["Software"]["valve"]["steam"]["apps"]
+	apps=stuff["UserLocalConfigStore"]["Software"]["Valve"]["Steam"]["apps"]
 	ids=sorted([int(x) for x in apps])
 	ids.remove(0)
 	return tuple(ids)
